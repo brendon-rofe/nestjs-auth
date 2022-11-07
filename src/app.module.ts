a2f68fec0a6f95872d5a357db70352377503fd2c
Bravo1B9 <b1@xgmi.com>
2022-11-07 11:36:46 +0000

    Initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'user',
+      password: 'password',
+      database: 'user_auth',
+      entities: [],
+      synchronize: true,
+      dropSchema: true
+    }),
+  ],
+})
+export class AppModule {}
